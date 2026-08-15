3ed983be22fa81693d3933a4221bcc413509e7bd
Rafail <Raf_0707@mail.ru>
2026-08-15 12:38:29 +0300

    fix(ui): load targeted LMS fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AppErrorBoundary } from './components/AppErrorBoundary';
 import './styles/index.css';
+import './styles/lms-fixes.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
